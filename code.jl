092fb626dc81554d30de9a8aa57120157b8a74f5
Kai Jennissen <kai.jennissen@statworx.com>
2020-01-20 23:01:28 +0100

    Algorithm 2.1

--- code.jl
+++ code.jl
@@ -7,20 +7,27 @@ struct spMatrix
     x
 end
 
-
-Adense = [4.6 2.3 0; 0 1.5 0; 3.0 0 2.7]
-x = [1; 3; 4]
-Adense*x
+# Algorithm 2.1 - sparse matrix time dense vector
+function sparse_mult(A, x)
+    n = size(x,1);
+    y = zeros(n);
+    nnz = length(A.x);
+    for i in 1:n
+        for j in (A.p[i]+1):(A.p[i+1])
+            row = A.i[j]
+            y[row] = y[row] + A.x[j]*x[i];
+        end
+    end
+    return y
+end
 
 A = spMatrix([0, 2, 4, 5], [1, 3, 1, 2, 3], [4.6, 3.0, 2.3, 1.5, 2.7])
+x = [1; 3; 5]
+sparse_mult(A,x) == [4.6 2.3 0; 0 1.5 0; 3.0 0 2.7] * x
 
-# Algorithm 2.1 - sparse matrix time dense vector
-y = zeros(3);
-n = size(x,1);
-nnz = length(A.x);
-for i in 1:n
-    for j in (A.p[i]+1):(A.p[i+1])
-        row = A.i[j]
-        y[row] = y[row] + A.x[j]*x[i];
-    end
-end
+
+
+
+
+
+# Algorithm 2.2 - 
